0927f19d24f596eeaa5def5e5b6038819ebfc7b8
‘zuishuainanhai’ <1705902650@qq.com>
2022-11-13 20:10:20 +0800

    页面结构

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,8 +1,39 @@
+import { Route } from 'react-router-dom'
+import { TabBar } from 'antd-mobile'
+import styles from './index.module.scss'
+import Icon from '@/components/Icon'
+import { Outlet } from 'react-router-dom'
+
+// 菜单数据
+const tabs = [
+  { path: '/', icon: 'iconbtn_home', text: '首页' },
+  { path: '/question', icon: 'iconbtn_qa', text: '问答' },
+  { path: '/video', icon: 'iconbtn_video', text: '视频' },
+  { path: '/profile', icon: 'iconbtn_mine', text: '我的' },
+]
+
 const Layout = () => {
-return (
- <>
- <div>Layout</div>
- </>
- )
+  return (
+    <div className={styles.root}>
+      {/* 子路由挂载点 */}
+      <Outlet />
+
+      <TabBar className="tab-bar">
+        {tabs.map((item) => (
+          <TabBar.Item
+            key={item.path}
+            icon={(active) => (
+              <Icon
+                type={active ? `${item.icon}_sel` : item.icon}
+                className="tab-bar-item-icon"
+              />
+            )}
+            title={item.text}
+          />
+        ))}
+      </TabBar>
+    </div>
+  )
 }
+
 export default Layout
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,10 +2,33 @@
 import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
 import Test from '@/pages/test'
+// 导入子页面
+import Home from '@/pages/home'
+import Question from '@/pages/question'
+import Video from '@/pages/video'
+import Profile from '@/pages/profile'
 const routes = [
   {
     path: '/',
     element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/question',
+        element: <Question />,
+      },
+      {
+        path: '/video',
+        element: <Video />,
+      },
+      {
+        path: '/profile',
+        element: <Profile />,
+      },
+    ],
   },
   {
     path: '/login',
