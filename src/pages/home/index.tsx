2737a467ae455e56099db8d673394f66a525ac5e
‘zuishuainanhai’ <1705902650@qq.com>
2022-11-26 16:46:35 +0800

    渲染频道管理弹出层

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,9 +3,19 @@ import { useInitState } from '@/hooks/use-initial-state'
 import { getUserChannel } from '@/store/actions/home'
 import { Tabs } from 'antd-mobile'
 import styles from './index.module.scss'
-
+import { useState } from 'react'
+import { Popup } from 'antd-mobile'
+import Channels from './components/Channels'
 const Home = () => {
   const { userChannel } = useInitState(getUserChannel, 'home')
+  const [visible, setVisible] = useState(false)
+
+  const onChannelOpen = () => {
+    setVisible(true)
+  }
+  const onChannelClose = () => {
+    setVisible(false)
+  }
   return (
     <div className={styles.root}>
       {userChannel.length > 0 && (
@@ -19,8 +29,14 @@ const Home = () => {
       )}
       <div className="tabs-opration">
         <Icon type="iconbtn_search" />
-        <Icon type="iconbtn_channel" />
+        <Icon type="iconbtn_channel" onClick={onChannelOpen} />
       </div>
+      <Popup
+        visible={visible}
+        position="left"
+        className="channel-popup">
+        <Channels onClose={onChannelClose} />
+      </Popup>
     </div>
   )
 }
